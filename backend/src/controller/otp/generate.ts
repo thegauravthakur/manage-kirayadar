f723b72e66faeb08cda7c0ca1d77b9d163efd7c7
Gaurav Thakur <gthakur581@gmail.com>
2022-06-27 10:55:02 +0530

    handle the case where OTP is already present in DB

--- backend/src/controller/otp/generate.ts
+++ backend/src/controller/otp/generate.ts
@@ -5,7 +5,14 @@ import { sendEmail } from './send';
 export async function generateOTP(req: Request, res: Response) {
     const { email } = req.body;
     const otp = Math.floor(100000 + Math.random() * 900000);
-    const entry = await prismaClient.otp.create({ data: { otp, email } });
-    await sendEmail({ otp: entry.otp, to: email });
+    const otpAlreadyExists = await prismaClient.otp.findUnique({
+        where: { email },
+    });
+    if (otpAlreadyExists) {
+        prismaClient.otp.update({ where: { email }, data: { otp } });
+    } else {
+        await prismaClient.otp.create({ data: { otp, email } });
+    }
+    await sendEmail({ otp: otp, to: email });
     res.json({ errorMessage: null, data: null });
 }
