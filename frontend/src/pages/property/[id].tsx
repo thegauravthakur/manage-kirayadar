6fe0202c8ab5a4f069b0ac757b221949b5dbd8d3
Gaurav Thakur <gthakur581@gmail.com>
2022-07-04 19:08:53 +0530

    create ui components for a property

--- frontend/src/pages/property/[id].tsx
+++ frontend/src/pages/property/[id].tsx
@@ -3,11 +3,72 @@ import { getCookie } from 'cookies-next';
 import { getCurrentUser } from '../../helpers/userHelper';
 import { createEndpoint, fetchWithToken } from '../../helpers/fetchHelper';
 import { Property, Response } from '../../types';
+import { AiOutlinePlus } from 'react-icons/ai';
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
+import { useState } from 'react';
+import clsx from 'clsx';
 
-export default function DetailedProperty() {
+interface DetailedPropertyProps {
+    property: Property;
+}
+
+function SpaceCard() {
     return (
-        <div>
-            <p>yooo</p>
+        <div className='shadow border p-5 rounded-xl flex items-center justify-between'>
+            <div>
+                <h3 className='text-lg font-semibold text-secondary'>
+                    202 - A
+                </h3>
+                <p>First floor</p>
+            </div>
+            <button className='btn btn-outline btn-primary btn-sm'>
+                Manage
+            </button>
+        </div>
+    );
+}
+
+function CollapsableFloor() {
+    const [isExpanded, setIsExpanded] = useState(true);
+    return (
+        <div className={clsx('space-y-5')}>
+            <button
+                className='btn btn-outline btn-primary gap-3'
+                onClick={() => setIsExpanded(!isExpanded)}
+            >
+                First Floor
+                {isExpanded ? (
+                    <BiChevronDown size={22} />
+                ) : (
+                    <BiChevronUp size={22} />
+                )}
+            </button>
+            {isExpanded && (
+                <div className={clsx('grid grid-cols-2 gap-5 p-0')}>
+                    <SpaceCard />
+                    <SpaceCard />
+                    <SpaceCard />
+                    <SpaceCard />
+                    <SpaceCard />
+                    <SpaceCard />
+                </div>
+            )}
+        </div>
+    );
+}
+export default function DetailedProperty({ property }: DetailedPropertyProps) {
+    return (
+        <div className='p-5 space-y-5'>
+            <div className='flex justify-between'>
+                <h1 className='text-2xl font-semibold'>{property.name}</h1>
+                <button className='btn btn-primary btn-wide gap-2'>
+                    <AiOutlinePlus size={22} />
+                    Add New Space
+                </button>
+            </div>
+            <CollapsableFloor />
+            <CollapsableFloor />
+            <CollapsableFloor />
         </div>
     );
 }
@@ -32,11 +93,11 @@ export const getServerSideProps: GetServerSideProps = async ({
             const { data } = (await response.json()) as Response<{
                 properties: Property[];
             }>;
-            const hasAccess = data.properties.some(
+            const property = data.properties.find(
                 ({ id }) => Number(id) === Number(propertyId)
             );
-            if (hasAccess) {
-                return { props: {} };
+            if (property) {
+                return { props: { property } };
             }
             return { notFound: true };
         }
