eecdac822e8b929aa518abfa58a78025fe859720
Gaurav Thakur <gthakur581@gmail.com>
2022-07-12 20:46:35 +0530

    create a minimal profile view

--- frontend/src/pages/property/[propertyId]/space/[spaceId]/tenant/[tenantId].tsx
+++ frontend/src/pages/property/[propertyId]/space/[spaceId]/tenant/[tenantId].tsx
@@ -5,14 +5,32 @@ import {
 } from '../../../../../../helpers/fetchHelper';
 import { getCookie } from 'cookies-next';
 import { Tenant } from '../../../../../../types';
+import { AppBar } from '../../../../../../components/AppBar';
+import Image from 'next/image';
 
 interface TenantViewProp {
     tenant: Tenant;
 }
 function TenantView({ tenant }: TenantViewProp) {
     return (
-        <div>
-            <p>{tenant.name}</p>
+        <div className='bg-base-200 min-h-screen'>
+            <AppBar />
+            <div className='p-5 my-10 flex space-x-10 max-w-7xl mx-auto'>
+                <Image
+                    alt=''
+                    className='rounded-full'
+                    height={200}
+                    src='https://placeimg.com/200/200/people'
+                    width={200}
+                />
+                <div className='flex flex-col space-y-2 items-start justify-between'>
+                    <div>
+                        <h2 className='font-bold text-3xl'>{tenant.name}</h2>
+                        <p>{tenant.email}</p>
+                    </div>
+                    <button className='btn btn-outline'>Contact</button>
+                </div>
+            </div>
         </div>
     );
 }
