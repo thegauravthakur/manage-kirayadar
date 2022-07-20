e9580796f227ce153b2e7289cc299b37f802473b
Gaurav Thakur <gthakur581@gmail.com>
2022-07-20 21:56:47 +0530

    add authorization to other document controllers

--- backend/src/controller/documents/getAllDocuments.ts
+++ backend/src/controller/documents/getAllDocuments.ts
@@ -2,20 +2,27 @@ import type { Request, Response } from 'express';
 import { prismaClient } from '../../utils/server';
 import { z } from 'zod';
 import { hasAccessOnTenant } from '../../utils/jwt';
+import { sendError } from '../../utils/shared';
 
 const bodySchema = z.object({
-    tenantId: z.string(),
+    tenantId: z.number({ required_error: 'tenantId is required' }),
 });
 type BodySchema = z.infer<typeof bodySchema>;
 
 export async function getAllDocuments(req: Request, res: Response) {
-    const { tenantId } = req.body as BodySchema;
-    const hasAccess = await hasAccessOnTenant(req, Number(tenantId));
-    if (!hasAccess)
-        res.status(401).json({ data: null, errorMessage: 'unauthorized!' });
-    const documents = await prismaClient.document.findMany({
-        where: { tenantId: Number(tenantId) },
-        select: { name: true, id: true },
-    });
-    res.json({ data: { documents }, errorMessage: null });
+    try {
+        bodySchema.parse(req.body);
+        const { tenantId } = req.body as BodySchema;
+        const hasAccess = await hasAccessOnTenant(req, tenantId);
+        if (!hasAccess)
+            res.status(401).json({ data: null, errorMessage: 'unauthorized!' });
+        const documents = await prismaClient.document.findMany({
+            where: { tenantId: tenantId },
+            select: { name: true, id: true },
+        });
+        res.json({ data: { documents }, errorMessage: null });
+    } catch (error) {
+        const message = 'error occurred while fetching all documents';
+        sendError(res, error, message);
+    }
 }
--- backend/src/controller/documents/getFileFromS3.ts
+++ backend/src/controller/documents/getFileFromS3.ts
@@ -2,10 +2,28 @@ import { Request, Response } from 'express';
 import { sendFileFromS3 } from '../../utils/S3';
 import { sendError } from '../../utils/shared';
 import { prismaClient } from '../../utils/server';
+import { string, z } from 'zod';
+import { hasAccessOnDocument, hasAccessOnTenant } from '../../utils/jwt';
+
+const bodySchema = z.object({
+    documentId: string({ required_error: 'documentId is required' }),
+    tenantId: string({ required_error: 'tenantId is required' }),
+});
+
+type BodySchema = z.infer<typeof bodySchema>;
 
 export async function getFileFromS3(request: Request, response: Response) {
     try {
-        const { documentId, tenantId } = request.body;
+        bodySchema.parse(request.body);
+        const { documentId, tenantId } = request.body as BodySchema;
+        const hasAccess =
+            (await hasAccessOnTenant(request, Number(tenantId))) &&
+            (await hasAccessOnDocument(request, Number(documentId)));
+        if (!hasAccess) {
+            return response
+                .status(401)
+                .json({ data: null, errorMessage: 'unauthorized!' });
+        }
         const document = await prismaClient.document.findFirst({
             where: { id: Number(documentId), tenantId: Number(tenantId) },
         });
