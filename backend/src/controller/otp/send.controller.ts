3d23271d48b74670fde23f16ebce0a1e3d3f38a2
Gaurav Thakur <gthakur581@gmail.com>
2022-07-24 11:25:57 +0530

    uncomment code to send email

--- backend/src/controller/otp/send.controller.ts
+++ backend/src/controller/otp/send.controller.ts
@@ -16,12 +16,11 @@ export async function sendEmail({ otp, to }: SendEmailProps) {
         },
     });
 
-    // TODO Uncomment this later
-    // await transporter.sendMail({
-    //     from: '"Manage Kirayadar" <no-reply@managekirayadar.com>', // sender address
-    //     to,
-    //     subject: 'Your OTP to login to Manage Kirayadar', // Subject line
-    //     text: `${otp} is your OTP to login to Manage Kirayadar`,
-    //     html: `<b>${otp} is your OTP to login to Manage Kirayadar</b>`, // html body
-    // });
+    await transporter.sendMail({
+        from: '"Manage Kirayadar" <no-reply@managekirayadar.com>', // sender address
+        to,
+        subject: 'Your OTP to login to Manage Kirayadar', // Subject line
+        text: `${otp} is your OTP to login to Manage Kirayadar`,
+        html: `<b>${otp} is your OTP to login to Manage Kirayadar</b>`, // html body
+    });
 }
