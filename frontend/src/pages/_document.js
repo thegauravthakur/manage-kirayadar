fbccc996f5caff9d85309357463f3f7392ce50ae
Gaurav Thakur <gthakur581@gmail.com>
2022-07-25 13:52:13 +0530

    remove unnecessary files

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -3,13 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
     return (
         <Html lang='en'>
-            <Head>
-                {/*todo: remove this when we have https connection*/}
-                <meta
-                    content='upgrade-insecure-requests'
-                    httpEquiv='Content-Security-Policy'
-                />
-            </Head>
+            <Head />
             <body>
                 <Main />
                 <NextScript />
