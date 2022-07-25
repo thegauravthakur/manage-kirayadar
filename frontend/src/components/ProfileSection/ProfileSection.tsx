9f0fcec4ec07ebeb99b338f64e95c1d523762ec0
Gaurav Thakur <gthakur581@gmail.com>
2022-07-25 15:06:33 +0530

    add functionality to edit image visually

--- frontend/src/components/ProfileSection/ProfileSection.tsx
+++ frontend/src/components/ProfileSection/ProfileSection.tsx
@@ -1,19 +1,33 @@
-import Image from 'next/image';
-
+import { AiOutlineEdit } from 'react-icons/ai';
+import { useState } from 'react';
+import clsx from 'clsx';
 interface ProfileSectionProps {
     name: string;
 }
 
 export function ProfileSection({ name }: ProfileSectionProps) {
+    const [isHovered, setIsHovered] = useState(false);
     return (
         <div className='flex flex-col items-center space-y-5 shadow-md p-8 rounded-xl border bg-base-100'>
-            <Image
-                alt=''
-                className='rounded-full'
-                height={150}
-                src='https://placeimg.com/150/150/people'
-                width={150}
-            />
+            <div
+                className='h-32 w-32 rounded-full flex items-end justify-center overflow-hidden'
+                style={{
+                    backgroundImage: 'url(https://placeimg.com/150/150/people)',
+                }}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                <button
+                    className={clsx(
+                        'bg-base-200 bg-opacity-80 w-full rounded-b-full text-sm h-10 flex justify-center items-center',
+                        'transition-opacity duration-200 ease-in-out',
+                        isHovered ? 'opacity-100' : 'opacity-0'
+                    )}
+                >
+                    update
+                    <AiOutlineEdit size={18} />
+                </button>
+            </div>
             <div className='flex flex-col items-center'>
                 <h2 className='text-2xl font-semibold text-primary'>{name}</h2>
                 <p className='text-gray-500'>Some Placeholder</p>
