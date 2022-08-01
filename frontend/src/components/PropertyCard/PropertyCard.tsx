2e170f566a88cbeb3164641d36cd72d3c357b073
Gaurav Thakur <gthaku581@gmail.com>
2022-08-01 18:07:52 +0530

    add enhancements to the property card

--- frontend/src/components/PropertyCard/PropertyCard.tsx
+++ frontend/src/components/PropertyCard/PropertyCard.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { BsPeople } from 'react-icons/bs';
 import { pluralize } from '../../helpers/shared';
 
 interface PropertyCardProps {
@@ -16,9 +17,10 @@ export function PropertyCard({
     return (
         <div className='card w-full max-w-sm bg-base-100 shadow-xl h-60'>
             <div className='card-body uppercase p-5 sm:p-7'>
-                <div className='flex items-center justify-between'>
+                <div className='flex flex-col lg:flex-row items-start lg:items-center justify-between space-y-1'>
                     <h2 className='card-title'>{name}</h2>
-                    <span className='text-xs bg-green-600 text-base-100 py-1 px-2 rounded whitespace-nowrap'>
+                    <span className='text-xs bg-green-600 text-base-100 py-1 px-2 rounded whitespace-nowrap flex items-center'>
+                        <BsPeople className='mr-2' size={15} />
                         {totalTenants}{' '}
                         {pluralize('tenant', 'tenants', totalTenants)}
                     </span>
