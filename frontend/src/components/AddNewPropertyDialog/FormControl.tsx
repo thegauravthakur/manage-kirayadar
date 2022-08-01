e747fe16286453952b73ebe694fd519ffff0e45e
Gaurav Thakur <gthaku581@gmail.com>
2022-08-01 16:11:36 +0530

    create a new component for input box

--- frontend/src/components/AddNewPropertyDialog/FormControl.tsx
+++ frontend/src/components/AddNewPropertyDialog/FormControl.tsx
@@ -1,7 +1,6 @@
-import { FormLabel } from '../FormLabel';
-import clsx from 'clsx';
 import { FormState, UseFormRegister } from 'react-hook-form';
 import { CreateNewPropertySchema } from './AddNewPropertyDialog';
+import { InputBox } from '../UI/InputBox';
 
 interface FormControlProps {
     formState: FormState<CreateNewPropertySchema>;
@@ -11,56 +10,30 @@ export function FormControl({ formState, register }: FormControlProps) {
     const { errors } = formState;
     return (
         <>
-            <FormLabel
-                errorText={errors.name?.message}
+            <InputBox
+                error={errors.name?.message}
                 id='name'
-                labelText='What is name of this property?'
-            >
-                <input
-                    className={clsx(
-                        'input input-bordered input-primary input-md w-full',
-                        { 'input-error': !!errors.name }
-                    )}
-                    id='name'
-                    placeholder='Enter a name...'
-                    type='text'
-                    {...register('name')}
-                />
-            </FormLabel>
-            <FormLabel
-                errorText={errors.totalFloors?.message}
-                id='totalFloors'
-                labelText='How many floors are there?'
-            >
-                <input
-                    className={clsx(
-                        'input input-bordered input-primary input-md w-full',
-                        { 'input-error': !!errors.totalFloors }
-                    )}
-                    id='totalFloors'
-                    placeholder='total number of floors...'
-                    type='text'
-                    {...register('totalFloors', {
-                        valueAsNumber: true,
-                    })}
-                />
-            </FormLabel>
-            <FormLabel
-                errorText={errors.address?.message}
+                label='Property Name'
+                placeholder="Enter Property's Name"
+                registerForm={register('name')}
+                type='text'
+            />
+            <InputBox
+                error={errors.totalFloors?.message}
+                id='total-floors'
+                label='Total Floors'
+                placeholder='Enter Total Number of Floors'
+                registerForm={register('totalFloors', { valueAsNumber: true })}
+                type='number'
+            />
+            <InputBox
+                error={errors.address?.message}
                 id='address'
-                labelText='Where is this property located?'
-            >
-                <input
-                    className={clsx(
-                        'input input-bordered input-primary input-md w-full',
-                        { 'input-error': !!errors.address }
-                    )}
-                    id='address'
-                    placeholder='Enter city name...'
-                    type='text'
-                    {...register('address')}
-                />
-            </FormLabel>
+                label='Address'
+                placeholder='Where is this Property Located?'
+                registerForm={register('address')}
+                type='address'
+            />
         </>
     );
 }
---
+++ frontend/src/components/UI/InputBox/InputBox.tsx
@@ -0,0 +1,39 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+import clsx from 'clsx';
+
+interface InputBoxProps {
+    id: string;
+    label: string;
+    placeholder: string;
+    type: HTMLInputElement['type'];
+    error?: string;
+    registerForm?: UseFormRegisterReturn;
+}
+
+export function InputBox(props: InputBoxProps) {
+    const { type, id, registerForm, error, placeholder, label } = props;
+    return (
+        <div className='relative mt-2'>
+            <input
+                className={clsx(
+                    'border border-black h-14 w-full rounded-lg pl-4',
+                    'outline-none focus:border-blue-700 placeholder-gray-400',
+                    { 'border-red-600 focus:border-red-600': !!error }
+                )}
+                id={id}
+                placeholder={placeholder}
+                type={type}
+                {...registerForm}
+            />
+            <label
+                className='absolute top-[-8px] px-1.5 left-[11px] bg-white text-xs'
+                htmlFor={id}
+            >
+                {label}
+            </label>
+            <p className='text-[11px] ml-2 pb-1.5 mt-0.5 text-red-600'>
+                {!!error && '*' + error} &#8203;
+            </p>
+        </div>
+    );
+}
