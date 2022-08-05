dcdbeb128c08f3577a6f50f8b19ebba457326cd5
Gaurav Thakur <gthakur581@gmail.com>
2022-08-05 12:43:57 +0530

    create new select component

--- frontend/src/components/AddNewSpaceDialog/AddNewSpaceDialog.tsx
+++ frontend/src/components/AddNewSpaceDialog/AddNewSpaceDialog.tsx
@@ -1,23 +1,29 @@
-import { Dispatch, SetStateAction, useRef } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
 import clsx from 'clsx';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import { useMutation, useQueryClient } from 'react-query';
 import { AiOutlineClose } from 'react-icons/ai';
 import { createEmptyArray, numberToWord } from '../../helpers/pageHelper';
-import { createEndpoint, postWithToken } from '../../helpers/fetchHelper';
-import { useSession } from '../../hooks/useSession';
 import { FormInputBox } from '../UI/FormInputBox';
-import Select, { components } from 'react-select';
-import { val } from 'dom7';
+import { FormSelectBox } from '../UI/FormSelectBox';
+import FocusTrap from 'focus-trap-react';
+import { useCreateNewSpaceMutation } from '../../hooks/react-query/mutation/useCreateNewSpaceMutation';
 
 const formSchema = z.object({
     name: z.string().min(2, 'name should have at least 2 letter'),
     floor: z.number().min(1, ''),
 });
 
-type FormSchema = z.infer<typeof formSchema>;
+export type CreateNewSpaceSchema = z.infer<typeof formSchema>;
+
+const getOptions = (totalFloors: number) => {
+    const emptyArray = createEmptyArray(totalFloors - 1);
+    return emptyArray.map((value) => ({
+        value: numberToWord(value + 1) + ' floor',
+        key: value + 1,
+    }));
+};
 
 interface AddNewSpaceDialogProps {
     showDialog: boolean;
@@ -26,105 +32,54 @@ interface AddNewSpaceDialogProps {
     propertyId: number;
 }
 
-async function createNewSpace(
-    formData: FormSchema,
-    token: string,
-    propertyId: number
-) {
-    await postWithToken(createEndpoint('space/add'), token, {
-        ...formData,
-        propertyId,
-    });
-}
-
-// function SelectContainer({ errors, ...rest }: any) {
-//     const { children, ...r } = rest;
-//     return (
-//         <div className='relative mt-2'>
-//             <input
-//                 {...r}
-//                 className={clsx(
-//                     'border border-black h-14 w-full rounded-lg pl-4',
-//                     'outline-none focus:border-blue-700 placeholder-gray-400',
-//                     {
-//                         'border-red-600 focus:border-red-600':
-//                             !!errors.floor?.message,
-//                     }
-//                 )}
-//                 id='floor'
-//                 placeholder='floor'
-//                 type='text'
-//             />
-//             <label
-//                 className='absolute top-[-8px] px-1.5 left-[11px] bg-white text-xs'
-//                 htmlFor='floor'
-//             >
-//                 Select Floor
-//             </label>
-//             <p className='text-[11px] ml-2 pb-1.5 mt-0.5 text-red-600'>
-//                 {!!errors.floor?.message && '*' + errors.floor.message} &#8203;
-//             </p>
-//         </div>
-//     );
-// }
-
-const SelectContainer = ({ children, ...props }: any) => {
-    return (
-        <components.SelectContainer {...props} className=''>
-            {children}
-            <label className='absolute top-[-8px] px-1.5 left-[11px] bg-white text-xs'>
-                Floor
-            </label>
-        </components.SelectContainer>
-    );
-};
-
 export function AddNewSpaceDialog({
     showDialog,
     setShowDialog,
     totalFloors,
     propertyId,
 }: AddNewSpaceDialogProps) {
-    const emptyArray = createEmptyArray(totalFloors - 1);
     const {
         register,
         handleSubmit,
         reset,
+        setFocus,
         formState: { errors },
-    } = useForm<FormSchema>({
+    } = useForm<CreateNewSpaceSchema>({
         resolver: zodResolver(formSchema),
     });
-    const { token } = useSession();
-    const queryClient = useQueryClient();
-    const mutation = useMutation(
-        async (formData: FormSchema) => {
-            await createNewSpace(formData, token!, propertyId);
-        },
-        {
-            onSuccess: async () => {
-                await queryClient.invalidateQueries(['spaces', propertyId]);
-                reset();
-                setShowDialog(false);
-            },
-        }
-    );
-    const onSubmit = handleSubmit((formData) => {
-        mutation.mutate(formData);
+    const mutation = useCreateNewSpaceMutation(propertyId, () => {
+        reset();
+        setShowDialog(false);
     });
-    const options = emptyArray.map((value) => ({
-        label: numberToWord(value + 1) + ' floor',
-        value: numberToWord(value + 1) + ' floor',
-    }));
-    const ref = useRef<HTMLDivElement>(null);
+    const options = getOptions(totalFloors);
+    const modalRef = useRef<HTMLDivElement>(null);
+    const onSubmit = handleSubmit((formData) => mutation.mutate(formData));
+
+    useEffect(() => {
+        if (showDialog) {
+            setFocus('name');
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+            reset();
+        }
+    }, [reset, setFocus, showDialog]);
+
     return (
-        <div
-            ref={ref}
-            className={clsx('modal', {
-                'modal-open': showDialog,
-            })}
-        >
-            <form className='modal-box space-y-5' onSubmit={onSubmit}>
-                <div>
+        <FocusTrap active={showDialog}>
+            <div
+                ref={modalRef}
+                className={clsx('modal', {
+                    'modal-open': showDialog,
+                })}
+                onKeyDown={({ key }) => {
+                    if (key === 'Escape') setShowDialog(false);
+                }}
+            >
+                <form
+                    className='modal-box w-full mx-2 px-3.5 sm:px-5 max-w-md'
+                    onSubmit={onSubmit}
+                >
                     <div className='flex justify-between mb-5'>
                         <h2 className='font-bold text-lg'>Add New Space</h2>
                         <button
@@ -146,59 +101,25 @@ export function AddNewSpaceDialog({
                         registerForm={register('name')}
                         type='text'
                     />
-
-                    <Select
-                        components={{
-                            SelectContainer,
-                        }}
-                        menuPortalTarget={ref.current}
+                    <FormSelectBox
+                        error={errors.floor?.message}
+                        id='floor'
+                        label='Chose Floor'
                         options={options}
-                        styles={{
-                            control: () => ({
-                                border: '1px solid black',
-                                cursor: 'pointer',
-                                padding: '8px 0 8px 12px',
-                                borderRadius: '0.5rem',
-                                display: 'flex',
-                                ':focus-within': {
-                                    borderColor: 'rgb(29 78 216)',
-                                },
-                            }),
-                        }}
-                        onChange={(v) => console.log(v)}
+                        registerForm={register('floor', {
+                            valueAsNumber: true,
+                        })}
                     />
-                    {/*<FormLabel*/}
-                    {/*    errorText={errors.floor?.message}*/}
-                    {/*    id='floor'*/}
-                    {/*    labelText='What floor is this space on?'*/}
-                    {/*>*/}
-                    {/*    <select*/}
-                    {/*        className={clsx(*/}
-                    {/*            'select select-primary font-normal w-full',*/}
-                    {/*            {*/}
-                    {/*                'select-error': !!errors.floor,*/}
-                    {/*            }*/}
-                    {/*        )}*/}
-                    {/*        defaultValue={1}*/}
-                    {/*        {...register('floor', { valueAsNumber: true })}*/}
-                    {/*    >*/}
-                    {/*        {emptyArray.map((value) => (*/}
-                    {/*            <option key={value} value={value + 1}>*/}
-                    {/*                {numberToWord(value + 1)} floor*/}
-                    {/*            </option>*/}
-                    {/*        ))}*/}
-                    {/*    </select>*/}
-                    {/*</FormLabel>*/}
-                </div>
-                <button
-                    className={clsx('btn btn-primary btn-block', {
-                        loading: mutation.isLoading,
-                    })}
-                    type='submit'
-                >
-                    create new space
-                </button>
-            </form>
-        </div>
+                    <button
+                        className={clsx('btn btn-primary btn-block', {
+                            loading: mutation.isLoading,
+                        })}
+                        type='submit'
+                    >
+                        create new space
+                    </button>
+                </form>
+            </div>
+        </FocusTrap>
     );
 }
---
+++ frontend/src/components/UI/FormSelectBox/FormSelectBox.tsx
@@ -0,0 +1,49 @@
+import clsx from 'clsx';
+import { UseFormRegisterReturn } from 'react-hook-form';
+
+interface FormSelectBoxProps {
+    error?: string;
+    registerForm?: UseFormRegisterReturn;
+    id: string;
+    label: string;
+    options: { key: number; value: string }[];
+}
+
+export function FormSelectBox({
+    registerForm,
+    error,
+    label,
+    id,
+    options,
+}: FormSelectBoxProps) {
+    return (
+        <div className='relative mt-2'>
+            <select
+                className={clsx(
+                    'border border-black h-14 w-full rounded-lg pl-4',
+                    'outline-none focus:border-blue-700 placeholder-gray-400',
+                    {
+                        'border-red-600 focus:border-red-600': !!error,
+                    }
+                )}
+                id={id}
+                {...registerForm}
+            >
+                {options.map(({ key, value }) => (
+                    <option key={key} value={key}>
+                        {value}
+                    </option>
+                ))}
+            </select>
+            <label
+                className='absolute top-[-8px] px-1.5 left-[11px] bg-white text-xs'
+                htmlFor='floor'
+            >
+                {label}
+            </label>
+            <p className='text-[11px] ml-2 pb-1.5 mt-0.5 text-red-600'>
+                {!!error && '*' + error} &#8203;
+            </p>
+        </div>
+    );
+}
