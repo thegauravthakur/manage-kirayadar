b6c1b7e90edd4fa904409b2b102da30ed465f81a
Gaurav Thakur <gthakur581@gmail.com>
2022-08-10 12:12:13 +0530

    whitelist Rudra's email

--- backend/src/controller/user/createNewUser.ts
+++ backend/src/controller/user/createNewUser.ts
@@ -19,11 +19,13 @@ const UserData = z.object({
 
 type UserSchema = z.infer<typeof UserData>;
 
+const whitelist = ['gthakur581@gmail.com', 'rudra.kaniya.rk@gmail.com'];
+
 export async function createUser(req: Request, res: Response) {
     try {
         UserData.parse(req.body);
         const { name, password, email, otp } = req.body as UserSchema;
-        if (email !== 'gthakur581@gmail.com')
+        if (!whitelist.includes(email))
             return res.status(400).json({
                 errorMessage:
                     'sorry, this product is in preview currently, stay tuned!',
