22ad927476af2e9e50822f533a546e6d593042a4
Gaurav Thakur <gthakur581@gmail.com>
2022-08-14 19:32:35 +0530

    use new controller while creating property

--- frontend/src/hooks/react-query/mutation/useCreateNewPropertyMutation.tsx
+++ frontend/src/hooks/react-query/mutation/useCreateNewPropertyMutation.tsx
@@ -1,19 +1,17 @@
 import { useMutation, useQueryClient } from 'react-query';
-import { useSession } from '../../useSession';
 import { useSnackbar } from '../../zustand/useSnackbar';
-import { createEndpoint, postWithToken } from '../../../helpers/fetchHelper';
+import { createEndpoint, requestWithToken } from '../../../helpers/fetchHelper';
 import { CustomError } from '../../../types';
 import { CreateNewPropertySchema } from '../../../components/AddNewPropertyDialog';
 import { useGlobalSpinner } from '../../zustand/useGlobalSpinner';
 
-async function createNewProperty(
-    formData: CreateNewPropertySchema,
-    token: string
-) {
-    const response = await postWithToken(
-        createEndpoint('property/add'),
-        token,
-        formData
+async function createNewProperty(formData: CreateNewPropertySchema) {
+    const response = await requestWithToken(
+        createEndpoint('property/upsert', true),
+        {
+            body: formData,
+            method: 'POST',
+        }
     );
     const result = await response.json();
     if (!response.ok) throw result;
@@ -22,13 +20,12 @@ async function createNewProperty(
 const errorMessage = 'Error occurred while fetching all the properties';
 export function useCreateNewPropertyMutation(onSuccess: () => void) {
     const queryClient = useQueryClient();
-    const { token } = useSession();
     const { show } = useSnackbar();
     const spinner = useGlobalSpinner();
     return useMutation(
         async (formData: CreateNewPropertySchema) => {
             spinner.show();
-            return createNewProperty(formData, token!);
+            return createNewProperty(formData);
         },
         {
             onError: (error: CustomError) => {
@@ -37,7 +34,6 @@ export function useCreateNewPropertyMutation(onSuccess: () => void) {
             },
             onSuccess: async () => {
                 show('created a new property', 'success');
-                // todo: maybe instead of invalidating the cache, we can directly update the cache which will be quick
                 await queryClient.invalidateQueries('properties');
                 onSuccess();
             },
